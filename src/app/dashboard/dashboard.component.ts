b72654879cc814f3a5798068efbdcda52e8d5137
Kashif Minhaj <dailypctips@gmail.com>
2017-04-11 18:37:19 +0530

    red5: allow streaming a video using red5prosdk

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,65 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var red5prosdk: any;
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
+
+
 export class DashboardComponent implements OnInit {
+
+  private tokenInput: string;
+  private streamInitialized: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  setupStream() {
+    if(!this.tokenInput) {
+      return;
+    }
+
+    // Create a new instance of the WebRTC subcriber.
+    var subscriber = new red5prosdk.RTCSubscriber();
+
+    // Create a view instance based on video element id.
+    var viewer = new red5prosdk.PlaybackView('red5pro-subscriber');
+    // Attach the subscriber to the view.
+    viewer.attachSubscriber(subscriber);
+
+    var protocol = window.location.protocol;
+    var isSecure = protocol.charAt(protocol.length - 2);
+
+    // Using Chrome/Google TURN/STUN servers.
+    var iceServers = [{urls: 'stun:stun2.l.google.com:19302'}];
+
+    var that = this;
+
+    // Initialize
+    subscriber.init({
+        protocol: 'ws',
+        host: 'localhost',
+        port: 8081,
+        app: 'live',
+        streamName: this.tokenInput,
+        iceServers: iceServers,
+        subscriptionId: 'subscriber-' + Math.floor(Math.random() * 0x10000).toString(16),
+      })
+      .then(function() {
+        // Invoke the playback action
+        that.streamInitialized = true
+        return subscriber.play();
+      })
+      .catch(function(error) {
+        // A fault occurred while trying to initialize and subscribe to the stream.
+        console.error(error);
+      });
+
   }
 
 }
